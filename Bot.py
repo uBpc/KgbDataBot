1262ceef13bf838a9ecb2f0634bff4f5a3127443
uBpc <94536983+uBpc@users.noreply.github.com>
2021-11-18 18:32:50 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -2,7 +2,6 @@ import praw
 import matplotlib.pyplot as plt
 import datetime
 import time
-from urllib.error import HTTPError
 def sort_dict_by_value(d, reverse = False):
   return dict(sorted(d.items(), key = lambda x: x[1], reverse = reverse))
 
@@ -96,5 +95,5 @@ while True:
             "freq_words":{},
             "commenters":{}
         }
-    except HTTPError as e:
+    except:
         time.sleep(30)
